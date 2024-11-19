10bf1a1408a7ce78993701bb5e271bde7db7fe9c
JTP.Codeing <xbigbossxovergameplay@gmail.com>
2024-11-19 15:48:44 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -4,65 +4,82 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.header('kairung')
-#st.image("./pic/kairung.jpg")
+# Header section
+st.header('Kairung')
+
+# Three columns for displaying flower images and titles
 col1, col2, col3 = st.columns(3)
 with col1:
-st.header("Versicolor")
-st.image("./pic/iris1.jpg")
+    st.header("Versicolor")
+    st.image("./pic/iris1.jpg")
 with col2:
-st.header("Verginiga")
-st.image("./pic/iris2.jpg")
+    st.header("Verginica")
+    st.image("./pic/iris2.jpg")
 with col3:
-st.header("Setosa")
-st.image("./pic/iris3.jpg")
+    st.header("Setosa")
+    st.image("./pic/iris3.jpg")
+
+# Display statistics header with styled box
 html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
 </div>
 """
 st.markdown(html_7, unsafe_allow_html=True)
-st.markdown("")
+
+# Load data
 dt = pd.read_csv("./data/iris-3.csv")
 st.write(dt.head(10))
+
+# Calculate sums of columns
 dt1 = dt['petallength'].sum()
 dt2 = dt['petalwidth'].sum()
 dt3 = dt['sepallength'].sum()
 dt4 = dt['sepalwidth'].sum()
+
 dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+
+# Display bar chart based on a button
 if st.button("แสดงการจินตทัศน์ข้อมูล"):
-#st.write(dt.head(10))
-st.bar_chart(dx2)
-st.button("ไม่แสดงข้อมูล")
+    st.bar_chart(dx2)
 else:
-st.write("ไม่แสดงข้อมูล")
+    st.write("ไม่แสดงข้อมูล")
+
+# Prediction section header with styled box
 html_8 = """
 <div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>ทำนายข้อมูล</h5></center>
 </div>
 """
 st.markdown(html_8, unsafe_allow_html=True)
-st.markdown("")
+
+# Slider and number input for getting user input to predict flower species
 pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
 pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
 sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
 sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+
+# Prediction logic
 if st.button("ทำนายผล"):
-#st.write("ทำนาย")
-dt = pd.read_csv("./data/iris.csv")
-X = dt.drop('variety', axis=1)
-y = dt.variety
-Knn_model = KNeighborsClassifier(n_neighbors=3)
-Knn_model.fit(X, y)
-x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
-st.write(Knn_model.predict(x_input))
-out=Knn_model.predict(x_input)
-if out[0] == 'Setosa':
-st.image("./pic/iris1.jpg")
-elif out[0] == 'Versicolor':
-st.image("./pic/iris2.jpg")
+    dt = pd.read_csv("./data/iris.csv")
+    X = dt.drop('variety', axis=1)
+    y = dt.variety
+    Knn_model = KNeighborsClassifier(n_neighbors=3)
+    Knn_model.fit(X, y)
+
+    # Prepare input data for prediction
+    x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+    prediction = Knn_model.predict(x_input)
+
+    st.write(prediction)  # Display the prediction
+
+    # Display corresponding image based on the prediction
+    if prediction[0] == 'Setosa':
+        st.image("./pic/iris1.jpg")
+    elif prediction[0] == 'Versicolor':
+        st.image("./pic/iris2.jpg")
+    else:
+        st.image("./pic/iris3.jpg")
 else:
-st.image("./pic/iris3.jpg")
-else:
-st.write("ไม่ทำนาย")
+    st.write("ไม่ทำนาย")
