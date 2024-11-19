9a56927fe60e2a682d722b10a1825b954d2c7ec1
JTP.Codeing <xbigbossxovergameplay@gmail.com>
2024-11-19 14:48:33 +0700

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("hello")
 st.image("./img/bigboss.jpg")
-st.st.subheader("Jaturapat Polrob")
+st.subheader("Jaturapat Polrob")
 st.header("hello")
 st.subheader("hello")
 
