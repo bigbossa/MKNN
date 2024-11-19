f1158c183297d2f2a23d7ef39319bb148ccf7343
JTP.Codeing <xbigbossxovergameplay@gmail.com>
2024-11-19 15:47:45 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -9,13 +9,13 @@ st.header('kairung')
 col1, col2, col3 = st.columns(3)
 with col1:
 st.header("Versicolor")
-st.image("./img/iris1.jpg")
+st.image("./pic/iris1.jpg")
 with col2:
 st.header("Verginiga")
-st.image("./img/iris2.jpg")
+st.image("./pic/iris2.jpg")
 with col3:
 st.header("Setosa")
-st.image("./img/iris3.jpg")
+st.image("./pic/iris3.jpg")
 html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
@@ -59,10 +59,10 @@ x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
 st.write(Knn_model.predict(x_input))
 out=Knn_model.predict(x_input)
 if out[0] == 'Setosa':
-st.image("./img/iris1.jpg")
+st.image("./pic/iris1.jpg")
 elif out[0] == 'Versicolor':
-st.image("./img/iris2.jpg")
+st.image("./pic/iris2.jpg")
 else:
-st.image("./img/iris3.jpg")
+st.image("./pic/iris3.jpg")
 else:
 st.write("ไม่ทำนาย")
