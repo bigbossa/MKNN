2790ebf61434f65f3bcc41418527e4cb50fab20a
JTP.Codeing <xbigbossxovergameplay@gmail.com>
2024-11-19 14:48:01 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd 
 
 st.title("hello")
-st.image("/img/bigboss.jpg")
+st.image("./img/bigboss.jpg")
+st.st.subheader("Jaturapat Polrob")
 st.header("hello")
 st.subheader("hello")
 
