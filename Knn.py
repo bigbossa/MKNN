a47d80ca6eb95fb6368bd00a0a4213295ed26d26
JTP.Codeing <xbigbossxovergameplay@gmail.com>
2024-11-19 16:00:49 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.header('kairung')
-#st.image("./pic/kairung.jpg")
+st.image("./img/bigboss.jpg")
 col1, col2, col3 = st.columns(3)
 with col1:
  st.header("Versicolor")
